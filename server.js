9e6b7806295b4bf591e8bf34b225ee0693ee4f51
Ameer Brown <akeithbrown@gmail.com>
2016-04-10 09:46:39 -0700

    served body parser

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Routes
 app.get('/', function sanity(req, res){
