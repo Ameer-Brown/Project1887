834c6a8ce308608f4720409a3c9ccc15b1e8860f
Ameer Brown <akeithbrown@gmail.com>
2016-04-10 20:54:46 -0700

    seeded database

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/Project1887_test");
 
-var Album = require('./college');
+var College = require('./college');
 
 module.exports.College = College;
 module.exports.Alumni = require('./alumni');
