dbe958b68710b1d86024709edb425c2b6188559e
Ameer Brown <akeithbrown@gmail.com>
2016-04-10 21:40:46 -0700

    fixed col.html handlebars layout

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/Project1887_test");
+mongoose.connect("mongodb://localhost/Project1887");
 
 var College = require('./college');
 
