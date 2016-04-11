163ab82daf36a4192a8b6f83d0ea03d17c82cd41
Ameer Brown <akeithbrown@gmail.com>
2016-04-11 00:49:41 -0700

    fixed server, back up and running

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,8 +1,8 @@
 function index(req, res) {
   res.json({
-    message: "Welcome to tunely!",
-    documentation_url: "https://github.com/tgaff/tunely/api.md",
-    base_url: "http://tunely.herokuapp.com",
+    message: "Welcome to Project1887!",
+    documentation_url: "",
+    base_url: "",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
