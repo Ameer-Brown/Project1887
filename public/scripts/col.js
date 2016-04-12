dca5c7dbd91368f4e939d0b4e798e201cd683a6e
Ameer Brown <akeithbrown@gmail.com>
2016-04-11 19:25:53 -0700

    fixed the return home button, had to hit the end point / not the file home.html

--- public/scripts/col.js
+++ public/scripts/col.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
 });
 
 
-function renderCollege(colleges) {
-  console.log("colleges inside the render: ",colleges);
+function renderCollege(college) {
+  console.log("college inside the render: ",college);
   // Target the html of the template
   var collegeHtml = $('#college-template').html();
   //compile the aluni html into the handlebars template
   var collegeTemplate= Handlebars.compile(collegeHtml);
 
   //prepend our compiled handlebars 'college html' after entering the college handlebars template
-  var html = collegeTemplate({colleges:colleges});
+  var html = collegeTemplate(college);
   console.log(html);
   $('#college').append(html);
 }
